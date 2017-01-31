23579671243a66cae67e7d42f77cb0c7262df556
Drewry Pope <dezren39@users.noreply.github.com>
2017-01-30 21:06:56 -0600

    An example of overriding a base class method.
    This was a short example I made for a discussion in my .NET Application Development course.

---
+++ HowManyTires/HowManyTires/HowManyTires.Designer.vb
@@ -0,0 +1,65 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class HowManyTires
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lblAuto = New System.Windows.Forms.Label()
+        Me.lblMoto = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'lblAuto
+        '
+        Me.lblAuto.AutoSize = True
+        Me.lblAuto.Location = New System.Drawing.Point(34, 46)
+        Me.lblAuto.Name = "lblAuto"
+        Me.lblAuto.Size = New System.Drawing.Size(0, 13)
+        Me.lblAuto.TabIndex = 0
+        '
+        'lblMoto
+        '
+        Me.lblMoto.AutoSize = True
+        Me.lblMoto.Location = New System.Drawing.Point(34, 94)
+        Me.lblMoto.Name = "lblMoto"
+        Me.lblMoto.Size = New System.Drawing.Size(0, 13)
+        Me.lblMoto.TabIndex = 1
+        '
+        'HowManyTires
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(254, 161)
+        Me.Controls.Add(Me.lblMoto)
+        Me.Controls.Add(Me.lblAuto)
+        Me.MaximizeBox = False
+        Me.MaximumSize = New System.Drawing.Size(270, 200)
+        Me.MinimizeBox = False
+        Me.MinimumSize = New System.Drawing.Size(270, 200)
+        Me.Name = "HowManyTires"
+        Me.Text = "How Many Tires?"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents lblAuto As Label
+    Friend WithEvents lblMoto As Label
+End Class
---
+++ HowManyTires/HowManyTires/HowManyTires.vb
@@ -0,0 +1,20 @@
+﻿Public Class HowManyTires
+    Class Auto
+        Overridable Function Tires() As Decimal
+            Tires = 4 'Hard coding is bad, this is an example.
+        End Function
+    End Class
+    Class Moto
+        Inherits Auto
+        Overrides Function Tires() As Decimal
+            Tires = 2 'Still bad, maybe a couple less.
+        End Function
+    End Class
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim Automobile As Auto = New Auto
+        Dim Motorcycle As New Moto
+
+        lblAuto.Text = "An automobile has this many tires: " + Automobile.Tires().ToString
+        lblMoto.Text = "A motorcycle has this many tires: " + Motorcycle.Tires().ToString
+    End Sub
+End Class
